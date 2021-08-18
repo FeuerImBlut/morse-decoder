a59fcb1a3738da081fc46b3f7b8b919bade72745
FeuerImBlut <golubovich.as@gmail.com>
2021-08-18 13:33:52 +0300

    init: implement morse decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let result = '';
+	for (let i = 0; i < expr.length; i += 10) {
+		let decodedSymbol = expr.substring(i,i+10);
+		let key = '';
+		
+		if (decodedSymbol !== '**********') {
+			
+			for (let j = 0; j < decodedSymbol.length; j+=2 ) {
+				let partSymbol = decodedSymbol.substring(j,j+2);
+				
+				if (partSymbol !== '00') {
+					if (partSymbol === '10') {
+						key = key + '.';
+					}
+					else {
+						key = key + '-';
+					}
+				}
+			}
+		result = result + MORSE_TABLE[key];
+		}
+		else {
+					result = result + ' '; 
+		}
+	}
+	return result;
 }
 
 module.exports = {
